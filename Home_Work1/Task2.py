b13378c7c587106d8555b8827a373bc6bc29b34f
Arttemplab <artempkh@gmail.com>
2025-10-22 22:26:27 +0300

    Home work2

---
+++ Home_Work1/Task2.py
@@ -0,0 +1,33 @@
+my_name="Artem"
+my_age=39
+#Hi,
+#my name is  Artem
+#and I'm 39 years old!!!
+
+print('Hi,\nmy name is ', my_name, '\nand I\'m',my_age, 'years old', end='!!!\n')
+print('-----------------')
+#Hi!!! Ha-ha-ha
+#my name is Ha-ha-ha Artem Ha-ha-ha
+#and I'm Ha-ha-ha 39 Ha-ha-ha years old!!!
+print('Hi!!!', '\nmy name is' , my_name, '\nand I\'m',my_age, 'years old',sep=" Ha-ha-ha ", end='!!!\n')
+print('-----------------')
+#my
+#new
+#teacher
+print("my\nnew\nteacher")
+#1 2 3 4 5 6 7
+print(1,2,3,4,5,6,7)
+#1 2 3 4 5 6 7
+print(1 ,2 ,3 ,4 ,5 ,6 ,7)
+#1Elephant 2Elephant 3Elephant 4Elephant 5Elephant 6Elephant 7
+print(1 ,2 ,3 ,4 ,5 ,6 ,7, sep="Elephant ")
+#1Elephant 2Elephant 3Elephant 4Elephant 5Elephant 6Elephant 7Elephant
+print(1 ,2 ,3 ,4 ,5 ,6 ,7, sep="Elephant ", end="Elephant")
+#line1
+#line2
+#line3
+print('\nline1','\nline2','\nline3')
+#line1 	line2 	line3
+print('line1','\tline2','\tline3')
+
+
